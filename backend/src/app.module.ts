843cebb2caaa1eea9f6fb5f1dd57ee09c8f189ac
yosiarel <yosia.sihaloho@gmail.com>
2026-09-06 11:49:30 +0700

    chore : remove Unused Modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,8 +7,6 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/project.module';
-import { WalletModule } from './wallet/wallet.module';
-import { InvestmentModule } from './investment/investment.module';
 import { KoperasiModule } from './koperasi/koperasi.module';
 import { UploadModule } from './upload/upload.module';
 import { AdminModule } from './admin/admin.module';
@@ -25,8 +23,6 @@ import { AdminModule } from './admin/admin.module';
     AuthModule,
     UserModule,
     ProjectModule,
-    WalletModule,
-    InvestmentModule,
     KoperasiModule,
     UploadModule,
     AdminModule,
