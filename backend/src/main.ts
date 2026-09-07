74ff4d3ea337e95af7f1964549494e9dcf184e7b
yosiarel <yosia.sihaloho@gmail.com>
2026-09-07 14:10:03 +0700

    enhance : add gloval prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,9 +34,11 @@ async function bootstrap() {
     }),
   );
 
+  app.setGlobalPrefix('api/v1');
+
   // Setup Swagger
   const config = new DocumentBuilder()
-    .setTitle('AkarMakmur API')
+    .setTitle('AgroFund API')
     .setDescription('Micro-Capital Crowdfunding & Digital Cooperative API')
     .setVersion('1.0')
     .addBearerAuth()
